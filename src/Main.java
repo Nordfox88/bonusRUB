a0a95dba3f166170bf69b49981abf0eb5443c0f3
Павел Воронин <nordfox88@gmail.com>
2022-08-06 21:47:14 +0300

    not ignore

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int capital = 100;
+        int buy = 1500;
+
+
+        if (buy>1000) {
+            int gift = buy / 100;
+            int total = capital + gift + buy;
+            System.out.println("Итоговый счет " + total);
+            System.out.println("Бонусный счет " + gift);
+        }else {
+            int total = capital + buy;
+            System.out.println("Итоговый счет " + total + "р");
+            System.out.println("Бонусный счет 0 р");
+        }
+
+    }
+}
