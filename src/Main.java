725b11c25100d9d12e989633608d9ea7a51005ce
Павел Воронин <nordfox88@gmail.com>
2022-08-11 21:06:15 +0300

    two

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ public class Main {
         int buy = 1500;
 
 
-        if (buy>1000) {
+        if (buy > 1000) {
             int gift = buy / 100;
             int total = capital + gift + buy;
             System.out.println("Итоговый счет " + total);
             System.out.println("Бонусный счет " + gift);
-        }else {
+        } else {
             int total = capital + buy;
             System.out.println("Итоговый счет " + total + "р");
             System.out.println("Бонусный счет 0 р");
